44c85cb3c2bf1cd2cbbe6123be52e284fad18bbd
jingling <jingling@cse.unsw.edu.au>
2025-02-01 11:49:04 +1100

    Update option.rs

--- tests/unit_tests/option.rs
+++ tests/unit_tests/option.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 <Wei Li>.
+// Copyright (c) 2024 <Wei Li>. 
 //
 // This source code is licensed under the GNU license found in the
 // LICENSE file in the root directory of this source tree.
